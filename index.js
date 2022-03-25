83462440ad0007e1856d3be162603ca8ff64bd4e
Swise99 <samuel@inprof.com>
2022-03-25 02:14:37 -0400

    Http clock server node.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const port = 8989;
+var currentdate=new Date();
+var datetime = "The Current date is : " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + 
+                " Would You like to play a game of chess??";
+http= require("http");
+httpStatus=require("http-status-codes");
+app=http.createServer((request,response) => {
+    response.writeHead(httpStatus.OK, {
+        "Content-Type": "text/html"
+    });
+    let responseMessage="<h1>"+datetime+"</h1>";
+    response.write(responseMessage);
+    response.end();
+    console.log(`We sent the following to a goober: ${responseMessage}`);
+});
+app.listen(port);
+console.log(`Our magic time clock is listening on port number: ${port}`);
