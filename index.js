58aa11856c547100d95e99daadc0293d3123bea4
Swise99 <samuel@inprof.com>
2022-03-25 02:19:02 -0400

    Time cube now updates on refresh

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const port = 8989;
-var currentdate=new Date();
-var datetime = "The Current date is : " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds() + 
-                " Would You like to play a game of chess??";
 http= require("http");
 httpStatus=require("http-status-codes");
 app=http.createServer((request,response) => {
     response.writeHead(httpStatus.OK, {
         "Content-Type": "text/html"
     });
+    let currentdate=new Date();
+    let datetime = "The Current date is : " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + 
+                " Would You like to play a game of chess??";
     let responseMessage="<h1>"+datetime+"</h1>";
     response.write(responseMessage);
     response.end();
